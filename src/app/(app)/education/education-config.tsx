695578cb5762b0e8a53f2e686e7eb119d9ac6f9e
Yeva Galstyan <eva.gal0908@gmail.com>
2025-12-06 11:39:00 +0100

    vulnarability changes

--- src/app/(app)/education/education-config.tsx
+++ src/app/(app)/education/education-config.tsx
@@ -1,5 +1,5 @@
 const title = "My Education"
-const description = "Here is an overview of my academic history, including my bachelor’s and master’s degrees, Erasmus exchange programs and high school."
+const description = "Here is an overview of my academic history, including my bachelor’s and master’s degrees, and high school."
 const homeImage = "https://jrjiiflokgzzwuboodhp.supabase.co/storage/v1/object/public/profile-images/education.png"
 
 const educationConfig= {
